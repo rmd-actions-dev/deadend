0471fca501db1ec1b96830a926218547eb760168
megener.id <matthew.egener@rcsdev38.newpace.ca>
2019-01-20 18:46:31 -0400

    Created a lib makefile that generates the .a files for libraries.

---
+++ src/default_lib.mk
@@ -0,0 +1,18 @@
+# Wildcards to compile any .cc files against boost
+SOURCES := $(wildcard *.cc)
+LIBS    := $(wildcard ${DEADEND_SRC}/lib/*.a)
+OBJECTS := $(patsubst %.cc,%.o,$(SOURCES))
+DIR = $(shell basename $(CURDIR))
+
+include ${DEADEND_SRC}/default.mk
+
+.cc.o:
+	@${CXX} -c $<
+.PHONY: all
+all : $(OBJECTS)
+	@$(CXX) -o $(DIR) $(OBJECTS) $(LIBS)
+	@ar rsv ${DEADEND_SRC}/lib/$(DIR).a $(OBJECTS) > /dev/null
+
+.PHONY: clean
+clean :
+	@$(RM) $(DIR) $(OBJECTS)
